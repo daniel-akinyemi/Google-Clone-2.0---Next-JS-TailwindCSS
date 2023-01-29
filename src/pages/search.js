d48563518285518b720e6f20724cd2b4c4f03447
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-01-29 18:09:44 +0100

    App Build Logic Complete 1.0

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,16 +1,27 @@
 import Header from "components/Header"
 import Head from "next/head"
 import { API_KEY, CONTEXT_KEY } from "keys"
+import Response from "Response"
+import Router, { useRouter } from "next/router"
+import SearchResults from "components/SearchResults"
+
+
+
 const Search = ({results}) => {
-  console.log(results)
+
+const router = useRouter();
+
+
+  console.log(results) 
   return (
     <div>
         <Head>
-            <title>Search Results</title>
+            <title>{router.query.term} - Google Search</title>
         </Head>
         {/* header  */}
         <Header/>
         {/* search results */}
+        <SearchResults results={results}/>
     </div>
   )
 }
@@ -19,8 +30,9 @@ export default Search
 
 export async function getServerSideProps(context){
   const useDummyData = false;
+  const startIndex = context.query.start || "0";
 
-  const data = await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`).then(response => response.json());
+  const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`).then(response => response.json());
 
   //After the SERVER has rendered... Pass teh results to the client...
   return{
