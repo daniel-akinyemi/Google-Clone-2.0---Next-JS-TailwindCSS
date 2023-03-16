b3db02ffa13a6fd5b676d89e34dcd07359086557
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-03-16 22:45:07 +0100

    headper options heroicons update

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -11,12 +11,12 @@ const HeaderOptions = () => {
   return (
     <div className="flex w-full text-gray-700 justify-evenly text-sm lg:text-base lg:justify-start lg:space-x-36 lg:pl-52 border-b-[1px]">
         <div className="flex space-x-6">
-            <HeaderOption Icon={MagnifyingGlassIcon} title='All' selected/>
+            {/* <HeaderOption Icon={MagnifyingGlassIcon} title='All' selected/>
             <HeaderOption Icon={PhotoIcon} title='Images'/>
             <HeaderOption Icon={PlayIcon} title='Videos'/>
             <HeaderOption Icon={NewspaperIcon} title='News'/>
             <HeaderOption Icon={MapIcon} title='Map'/>
-            <HeaderOption Icon={EllipsisHorizontalIcon} title='More'/>
+            <HeaderOption Icon={EllipsisHorizontalIcon} title='More'/> */}
 
 
         </div>
